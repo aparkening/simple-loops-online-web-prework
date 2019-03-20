f7553888a8691cb7187891ce805f2eabe0f10c7a
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-20 15:58:06 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -45,8 +45,7 @@ end
 
 # Loop using for
 def for_iterator(number_of_times)
-  range = 1..number_of_times
-  for counter in range
+  for counter in 1..number_of_times
     phrase = "Welcome to Flatiron School's Web Development Course!"
   end
 end
